5d1b8b46b28fd14a2b504b9482f329e2543c719c
Hayot Olivier <olivier.hayot.dev@gmail.com>
2024-10-10 19:27:50 +0200

    check auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,44 @@
-import prisma from "@/lib/prisma";
+'use client'
+import { useState } from 'react';
+import useAuth from '@/hooks/use-auth';
 
 export default async function Home() {
+	return <Login />
+}
 
-	return <p>hello</p>
+export function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login } = useAuth();
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      alert('Connexion réussie !');
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Connexion</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Se connecter</button>
+    </form>
+  );
 }
---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+export default function useAuth() {
+  const [user, setUser] = useState<string|undefined>(undefined);
+
+  useEffect(() => {
+    // Vérifier s'il y a un token dans localStorage
+    const token = localStorage.getItem('token');
+    if (token) {
+      setUser(token); // Vous pouvez décoder le token si besoin
+    }
+  }, []);
+
+  const login = async (email: string, password: string) => {
+    const res = await fetch('/weight-tracker/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      localStorage.setItem('token', data.token);
+      setUser(data.token);
+    } else {
+      throw new Error(data.message);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setUser(undefined);
+  };
+
+  return { user, login, logout };
+}
